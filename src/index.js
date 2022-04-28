77abc2aded6dc87fdfd4085c4032a6db1acc9f2e
Isaiah <ijandicos90@gmail.com>
2022-04-28 16:01:00 -0400

    Complete Assignment

--- src/index.js
+++ src/index.js
@@ -7,14 +7,20 @@ const init = () => {
   
       fetch(`http://localhost:3000/movies/${input.value}`)
       .then(response => response.json())
-      .then(data => {
-        const title = document.querySelector('section#movieDetails h4');
-        const summary = document.querySelector('section#movieDetails p');
-  
-        title.innerText = data.title;
-        summary.innerText = data.summary;
-      
-      });
+      .then(data => { 
+        let title = document.querySelector('section#movieDetails h4');
+        let summary = document.querySelector('section#movieDetails p')
+       if(data.title === undefined) {
+         title.innerText = "Sorry, data not found"
+         summary.innerText = "Sorry, data not found"
+        }
+        else {
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+          }
+         console.log( typeof data.title)
+      })
+      // hey next time do an if else buddy
     });
   }
   
